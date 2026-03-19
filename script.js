11eae13797001e254accfab60a4321293da84309
Hardo32 <252446776+Hardo32@users.noreply.github.com>
2026-03-20 00:19:48 +0200

    event update2

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-function galeryImgZoom(e) {
-  e.classList.toggle("gallImgFullSize");
-}
+const galeryImages = document.querySelectorAll(".galerii__list > li");
+let galeryclicking = false;
+
+const pastEventImages = document.querySelectorAll(".pastEventImages > li");
+let pastEventclicking = false;
+
+galeryImages.forEach((im) => {
+  im.addEventListener("click", function (e) {
+    galeryImages.forEach((img) => {
+      img.classList.remove("imgFullSize");
+    });
+    galeryclicking = !galeryclicking;
+    if (galeryclicking) {
+      e.target.classList.add("imgFullSize");
+    } else {
+      e.target.classList.remove("imgFullSize");
+    }
+  });
+});
+
+pastEventImages.forEach((im) => {
+  im.addEventListener("click", function (e) {
+    pastEventImages.forEach((img) => {
+      img.classList.remove("imgFullSize");
+    });
+    pastEventclicking = !pastEventclicking;
+    if (pastEventclicking) {
+      e.target.classList.add("imgFullSize");
+    } else {
+      e.target.classList.remove("imgFullSize");
+    }
+  });
+});
